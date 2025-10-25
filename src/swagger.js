c934f50c006a7399c03c848fb9b8762a5e7b40f8
ST-Jhon12 <jhonptt84@gmail.comm>
2025-10-25 14:53:04 -0500

    enviando nuevos datos

--- src/swagger.js
+++
@@ -1,3 +0,0 @@
-import swaggerJsdoc from 'swagger-jsdoc'; 
-const swaggerOptions = { definition: { openapi: '3.0.0', info: { title: 'Mi API REST', version: '1.0.0' }, servers: [{ url:
-'http://localhost:3000' }] }, apis: ['./routes/*.js'] }; export const swaggerSpec = swaggerJsdoc(swaggerOptions);
