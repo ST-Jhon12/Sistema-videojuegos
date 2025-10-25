9b6c0182d6acb0dc1871ad95daa6a8a4195cd30b
ST-Jhon12 <jhonptt84@gmail.comm>
2025-10-25 15:42:01 -0500

    enviando nuevos datos

--- src/backend/swagger.js
+++ src/backend/swagger.js
@@ -1,3 +1,21 @@
-import swaggerJsdoc from 'swagger-jsdoc'; 
-const swaggerOptions = { definition: { openapi: '3.0.0', info: { title: 'Mi API REST', version: '1.0.0' }, servers: [{ url:
-'http://localhost:3000' }] }, apis: ['./routes/*.js'] }; export const swaggerSpec = swaggerJsdoc(swaggerOptions);
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: { 
+        title: "API REST con Express, Prisma y PostgreSQL", 
+        version: "1.0.0",
+        description:"Documentacion de API REST con Express, Prisma y PostgreSQL",
+        contact:{
+            email:"jhonptt84@gmail.com.com"
+        }
+    },
+    servers: [{ 
+        url: "http://localhost:3000",
+        description:"Servidor de Desarrollo" 
+    }],
+  },
+  apis: ["./routes/*.js"],
+};
+export const swaggerSpec = swaggerJsdoc(swaggerOptions);
